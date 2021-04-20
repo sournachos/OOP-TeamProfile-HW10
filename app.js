e1cc9e494f196ac256c93293660c16ca8071f962
sournachos <joserivera18100@gmail.com>
2021-04-20 00:59:56 -0500

    Having some trouble with displaying the questions. The issues lies in the init function, when I only run mainQs() inside of init() it runs but when I add the other functions it stops working. I am assuming it's in Javascript's async nature to read it like that.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const manager = require("./lib/Manager");
+// const manager = require("./lib/Manager");
 // const Engineer = require("./lib/Engineer");
 // const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
@@ -19,56 +19,70 @@ function mainQs(){ inquirer.prompt([
             {type:'input', message:"Enter Your Team Manager's Email:", name:'managerEmail'},
                 {type:'input', message:"Enter Your Team Manager's Office Number:", name:'managerOfficeNumber'},
 ]).then((res)=> {
-        //const managerAnswers = [res.managerName, res.managerId, res.managerEmail, res.managerOfficeNumber]
         const managerHtml = `<div class="team-area mx-2 my-4 w-30 bg-secondary text-light text-center rounded-lg">
-${res.managerName} <br> <strong>Manager</strong> <br>
-<table class="table table-dark table-hover">
-<tr><td>ID: ${res.managerId}</td></tr><br>
-<tr><td>Email: <a href="mailto:${res.managerEmail}">${res.managerEmail}</a></td></tr>
-<tr><td>Office Number: ${res.managerOfficeNumber}</td></tr>
-</table>
-</div>`
-        // fs.readFile('templates\main.html',(err)=>{console.error(err)})
+                                ${res.managerName} <br> <strong>Manager</strong> <br>
+                                <table class="table table-dark table-hover">
+                                <tr><td>ID: ${res.managerId}</td></tr><br>
+                                <tr><td>Email: <a href="mailto:${res.managerEmail}">${res.managerEmail}</a></td></tr>
+                                <tr><td>Office Number: ${res.managerOfficeNumber}</td></tr>
+                                </table>
+                                </div>`
         fs.appendFile('templates/main.html', managerHtml, (err)=>{console.error(err)})
-        // const teamMembers = document.getElementById("teamMembers")
-        // teamMembers.appendChild(manager(managerAnswers))
-
 })}
 
-function addEngOrInt(){ inquire.prompt([{type:'checkbox', message:'Do you want to add a Team Engineer or Team Intern?', choices:['Engineer', 'Intern', 'No Thanks, I Am Finished Building My Team ;)'], name:'EngOrInt'}])}
 
-function IfAddEng(){ inquirer.prompt([
+
+function addEngOrInt(){ inquire.prompt([{type:'checkbox', message:'Do you want to add a Team Engineer or Team Intern?', choices:['Engineer', 'Intern', 'No Thanks, I Am Finished Building My Team ;)'], name:'EngOrIntChoice'}])}
+
+
+
+function AddEng(){ inquirer.prompt([
     {type:'input', message:"Enter Your Team Engineer's Name:", name:'engName'},
         {type:'number', message:"Enter Your Team Engineer's Employee ID:", name:'engId'},
             {type:'input', message:"Enter Your Team Engineer's Email:", name:'engEmail'},
                 {type:'input', message:"Enter Your Team Engineer's Github Username:", name:'engGit'}
-])}
+]).then((res)=> {
+        const engineerHtml = `<div class="team-area mx-2 my-4 w-30 bg-secondary text-light text-center rounded-lg">
+                                ${res.engName} <br> <strong>Engineer</strong> <br>
+                                <table class="table table-dark table-hover">
+                                <tr><td>ID: ${res.engId}</td></tr><br>
+                                <tr><td>Email: <a href="mailto:${res.engEmail}">${res.engEmail}</a></td></tr>
+                                <tr><td>Github: <a href="https://github.com/${res.engGit}">${res.engGit}</a></td></tr>
+                                </table>
+                                </div>`
+        // fs.readFile('templates\main.html',(err)=>{console.error(err)})
+        fs.appendFile('templates/main.html', engineerHtml, (err)=>{console.error(err)})
+})}
 
-function IfAddInt(){ inquirer.prompt([
+function AddInt(){ inquirer.prompt([
     {type:'input', message:"Enter Your Team Intern's Name:", name:'intName'},
         {type:'number', message:"Enter Your Team Intern's Employee ID:", name:'intId'},
             {type:'input', message:"Enter Your Team Intern's Email:", name:'intEmail'},
                 {type:'input', message:"Enter Your Team Intern's School Name:", name:'intSchool'}
-])}
+]).then((res)=> {
+        const internHtml = `<div class="team-area mx-2 my-4  w-30  bg-secondary text-light text-center rounded-lg">
+                                ${res.intName} <br> <strong>Intern</strong> <br>
+                                <table class="table table-dark table-hover">
+                                <tr><td>ID: ${res.intId}</td></tr><br>
+                                <tr><td>Email: <a href="mailto:${res.intEmail}">${res.intEmail}</a></td></tr>
+                                <tr><td>School: ${res.intSchool}</td></tr>
+                                </table>
+                                </div>`
+        // fs.readFile('templates\main.html',(err)=>{console.error(err)})
+        fs.appendFile('templates/main.html', internHtml, (err)=>{console.error(err)})
+})}
 
 
-//Different Maybes/Possibilities
-// (addEngOrInt.choices === 'Engineer') ? inquirer.prompt(IfAddEng) :  
-
+//Function to (init)ialize all the code in the desired order
 function init(){
     mainQs()
+    addEngOrInt()
+    if(res.EngOrIntChoice === 'Engineer'){(AddEng()).then(addEngOrInt())}
+    else if(res.EngOrIntChoice === 'Intern'){(AddInt()).then(addEngOrInt())}
+    else{console.log('Your team page has been created. Take a look at main.html to check it out.')}
 }
 
-
-
-
-
-
-init()
-
-
-
-
+init() //Calls the function above to run
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
